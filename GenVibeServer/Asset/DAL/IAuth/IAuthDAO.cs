7fdf971c4e9e3a0d0071804251e9902b8699281a
Cong Chinh(Assin) <126130000+AscAssin@users.noreply.github.com>
2024-08-04 21:00:13 +0700

    Add interface of AuthDAO

---
+++ GenVibeServer/Asset/DAL/IAuth/IAuthDAO.cs
@@ -0,0 +1,11 @@
+﻿using GenVibeServer.Asset.DTO.Common;
+
+namespace GenVibeServer.Asset.DAL.IAuth
+{
+    public interface IAuthDAO
+    {
+        public string Error { get; set; }
+        public bool Login(string username, string password);
+        public bool Register(UserDTO user);
+    }
+}
