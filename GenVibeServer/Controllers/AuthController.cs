d7117ffa4e1b1ac7f5178c775e5198c9f6b29fb9
Cong Chinh(Assin) <126130000+AscAssin@users.noreply.github.com>
2024-08-04 20:58:59 +0700

    Add controller of Auth functions

---
+++ GenVibeServer/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using GenVibeServer.Asset.BUS.Auth;
+using GenVibeServer.Asset.DTO.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace GenVibeServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public string? Error { get; set; }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            AuthBUS bus = AuthBUS.Instance;
+            this.Error = bus.Error;
+            bool status = bus.Login(username, password);
+            if (status)
+            {
+                return Ok();
+            }
+
+            return NotFound(Error);
+        }
+    }
+}
