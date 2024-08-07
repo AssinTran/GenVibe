406f8dc90414d5377778d1c1aa6989a2bb2c8dc7
Cong Chinh(Assin) <126130000+AscAssin@users.noreply.github.com>
2024-08-07 15:08:08 +0700

    Add getter and setter of property User

--- GenVibeServer/Asset/DAL/IAuth/IAuthDAO.cs
+++ GenVibeServer/Asset/DAL/IAuth/IAuthDAO.cs
@@ -5,6 +5,7 @@ namespace GenVibeServer.Asset.DAL.IAuth
     public interface IAuthDAO
     {
         public string Error { get; set; }
+        public string User { get; set; }
         public bool Login(string username, string password);
         public bool Register(UserDTO user);
     }
