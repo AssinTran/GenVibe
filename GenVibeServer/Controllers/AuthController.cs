b31cf7694b2f18bca51fc6b6d14ebb0de9caac0b
Cong Chinh(Assin) <126130000+AscAssin@users.noreply.github.com>
2024-08-07 15:12:38 +0700

    Modify function Login and Register - return value success

--- GenVibeServer/Controllers/AuthController.cs
+++ GenVibeServer/Controllers/AuthController.cs
@@ -12,17 +12,39 @@ namespace GenVibeServer.Controllers
     {
         public string? Error { get; set; }
 
-        [HttpPost]
+        [HttpPost("login")]
         public IActionResult Login(string username, string password)
         {
-            AuthBUS bus = AuthBUS.Instance;
-            this.Error = bus.Error;
+            AuthBUS bus = AuthBUS.Instance;            
             bool status = bus.Login(username, password);
+            this.Error = bus.Error;
             if (status)
             {
-                return Ok();
+                return Ok(bus.User);
             }
 
+            return NotFound(Error);
+        }
+
+        [HttpPost("register")]
+        public IActionResult Regitser(string username, string password, string email, string phone, string fullname)
+        {
+            UserDTO user = new()
+            {
+                Username = username,
+                Password = password,
+                Phone = phone,
+                FullName = fullname,
+                Email = email
+            };
+            AuthBUS authBUS = AuthBUS.Instance;
+            bool status = authBUS.Register(user);
+            this.Error = authBUS.Error;
+
+            if (status)
+            {
+                return Ok(authBUS.User);
+            }
             return NotFound(Error);
         }
     }
